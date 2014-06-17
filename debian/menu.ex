de58961b870aee23404eb05bc308a0740eda78b4
Kimpe Andy <andykimpe@gmail.com>
2014-06-17 04:06:05 +0200

    Veuillez saisir le message de validation pour vos modifications. Les lignes  commençant par '#' seront ignorées, et un message vide abandonne la validation.  Sur la branche master  Votre branche est à jour avec 'origin/master'.
     Modifications qui seront validées :
    	nouveau fichier: debian/README.Debian
    	nouveau fichier: debian/README.source
    	nouveau fichier: debian/changelog
    	nouveau fichier: debian/compat
    	nouveau fichier: debian/control
    	nouveau fichier: debian/copyright
    	nouveau fichier: debian/docs
    	nouveau fichier: debian/files
    	nouveau fichier: debian/init.d.ex
    	nouveau fichier: debian/install
    	nouveau fichier: debian/manpage.1.ex
    	nouveau fichier: debian/manpage.sgml.ex
    	nouveau fichier: debian/manpage.xml.ex
    	nouveau fichier: debian/menu.ex
    	nouveau fichier: debian/postinst.ex
    	nouveau fichier: debian/postrm.ex
    	nouveau fichier: debian/preinst.ex
    	nouveau fichier: debian/prerm.ex
    	nouveau fichier: debian/rules
    	nouveau fichier: debian/source/format
    	nouveau fichier: debian/test.cron.d.ex
    	nouveau fichier: debian/test.debhelper.log
    	nouveau fichier: debian/test.default.ex
    	nouveau fichier: debian/test.doc-base.EX
    	nouveau fichier: debian/test.substvars
    	nouveau fichier: debian/watch.ex

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(test):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="test" command="/usr/bin/test"
---
+++ debian/test.default.ex
@@ -0,0 +1,10 @@
+# Defaults for test initscript
+# sourced by /etc/init.d/test
+# installed at /etc/default/test by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
