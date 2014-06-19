0440df82b7d9b29e3bdf209516895fb92f6f2997
Kimpe Andy <andykimpe@gmail.com>
2014-06-19 21:25:38 +0200

    Create anti-adblock-plus-uptobox.meta.js

---
+++ anti-adblock-plus-uptobox.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name Anti-AntiBlock Plus
+// @description Skip a lot of anti-adblock.
+// @author floxflob andykimpe (update uptobox and post github)
+// @homepageURL https://github.com/andykimpe/test/blob/master/anti-adblock-plus-uptobox
+// @updateURL https://raw.githubusercontent.com/andykimpe/test/master/anti-adblock-plus-uptobox.meta.js
+// @downloadURL https://raw.githubusercontent.com/andykimpe/test/master/anti-adblock-plus-uptobox.user.js
+// @license GPL version 3
+// @include *
+// @exclude http*://*google.*
+// @exclude http*://*.gmail.*
+// @grant unsafeWindow
+// @grant GM_addStyle
+// @version 2.1.3
+// @run-at document-start
+// ==/UserScript==
